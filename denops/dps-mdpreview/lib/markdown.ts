4ce5a4d5809ac8ee2680a02292b6470697820c78
Kota Kato <peony.btn@gmail.com>
2021-09-25 19:21:59 +0900

    リンクを新規タブで開くように

--- denops/dps-mdpreview/lib/markdown.ts
+++ denops/dps-mdpreview/lib/markdown.ts
@@ -19,6 +19,33 @@ export default class Markdown {
         return "";
       },
     });
+    const defaultRender = this.md.renderer.rules.link_open ||
+      // @ts-ignore: 同下
+      function (tokens, idx, options, _, self) {
+        return self.renderToken(tokens, idx, options);
+      };
+    this.md.renderer.rules.link_open = function (
+    // @ts-ignore: 型定義が死ぬほど面倒いため
+      tokens,
+    // @ts-ignore: 型定義が死ぬほど面倒いため
+      idx,
+    // @ts-ignore: 型定義が死ぬほど面倒いため
+      options,
+    // @ts-ignore: 型定義が死ぬほど面倒いため
+      env,
+    // @ts-ignore: 型定義が死ぬほど面倒いため
+      self,
+    ) {
+      const aIndex = tokens[idx].attrIndex("target");
+      if (tokens[idx]["attrs"][0][1].match("http")) {
+        if (aIndex < 0) {
+          tokens[idx].attrPush(["target", "_blank"]);
+        } else {
+          tokens[idx].attrs[aIndex][1] = "_blank";
+        }
+      }
+      return defaultRender(tokens, idx, options, env, self);
+    };
     this.md.linkify.set({ fuzzyEmail: false });
   }
   toHTML(lines: string[]): string {
