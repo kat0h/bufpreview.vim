fdd71b0929b8ffe423a9178e4691308530f15b66
kuuote <znmxodq1@gmail.com>
2021-12-07 22:13:06 +0900

    Revert "Fix? #26"
    This reverts commit 96b324b3b6a5d6c2fa97b74f3fd63360331efc71.

--- denops/bufpreview/main.ts
+++ denops/bufpreview/main.ts
@@ -4,7 +4,6 @@ import Server from "./lib/server.ts";
 
 // 一度に開けるサーバーは一つ
 let server: Server | undefined;
-let serverInitializing: boolean = false
 
 export function main(denops: Denops) {
   denops.dispatcher = {
@@ -28,10 +27,6 @@ export function main(denops: Denops) {
 
       // サーバーを開く
       const openServer = async () => {
-        if (serverInitializing) {
-          return
-        }
-        serverInitializing = true
         // サーバーが既に開かれているなら
         if (server != undefined) {
           server.close();
@@ -52,7 +47,6 @@ export function main(denops: Denops) {
             console.log(`Server started on ${link}`);
           });
         }
-        serverInitializing = false
       };
 
       // サーバーを閉じる
